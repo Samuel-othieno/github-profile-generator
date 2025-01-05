46d1b1cc05421cce1086ae62ecf464389db9141c
Samuel Douglas <douglasothieno@gmail.com>
2025-01-05 14:35:13 +0300

    enhance index.html with meta tags for SEO; update InputForm and Profile styles for better responsiveness and dark mode support

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,25 +1,30 @@
-import React from "react";
 import "./InputForm.css";
 import { FaSearch } from "react-icons/fa";
+import PropTypes from 'prop-types';
 
 function InputForm({ username, handleChange, handleSubmit }) {
     return (
-        <div className="Input-wrapper">
-            <FaSearch id="search-icon" />
-
-            <form onSubmit={handleSubmit}>
-                
+        <div className="input-wrapper">
+            <form onSubmit={handleSubmit} className="input-form">
+                <FaSearch id="search-icon" />
                 <input
                     type="text"
                     value={username}
                     onChange={handleChange}
                     placeholder="Enter a GitHub username"
                 />
-            
             </form>
-
+            <footer className="footer">
+                <p>Created by Samuel Othieno</p>
+            </footer>
         </div>
     );
 }
+
+InputForm.propTypes = {
+    username: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+};
 
 export default InputForm;
