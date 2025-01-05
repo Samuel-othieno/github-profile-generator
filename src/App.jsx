242a7793a998b7c384b11179211abb3a1b955859
Samuel Douglas <douglasothieno@gmail.com>
2025-01-05 03:21:23 +0300

    implement theme toggle functionality and update styles for light/dark modes

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,62 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { FaSearch } from "react-icons/fa"
 import InputForm from "./InputForm";
 import Profile from "./Profile";
-
-
-function profileData(username) {
-
-  return fetch(`https://api.github.com/users/${username}`)
-    .then((response) => {
-
-      if (response.ok) {
-
-        return response.json();
-      } else {
-
-        throw new Error(response.status);
-      }
-    })
-    .catch((error) => {
-
-      console.error(error);
-      return null;
-    });
-}
+import { FaSun, FaMoon } from "react-icons/fa";
 
 function App() {
-
   const [username, setUsername] = useState("");
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [theme, setTheme] = useState("dark");
 
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
-  function handleChange(event) {
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
 
+  const handleChange = (event) => {
     setUsername(event.target.value);
-  }
+  };
 
-
-  async function handleSubmit(event) {
-
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
     setLoading(true);
-
     setError(null);
-
     setUserData(null);
 
     try {
-
       const data = await profileData(username);
-
       setUserData(data);
     } catch (error) {
-
       setError(error.message);
     } finally {
-
       setLoading(false);
     }
-  }
+  };
 
-  return ( 
-    <div className="App ">
+  const profileData = async (username) => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 
+  return (
+    <div className={`App ${theme}`}>
+      <button id="theme" onClick={toggleTheme}>
+        {theme === "dark" ? <FaSun /> : <FaMoon />}
+      </button>
       <div className="SearchBar">
         <h1>Find Your Github Profile</h1>
         <InputForm
